a881b71dec66ff14153cb891a02e6cdeebfa23a7
md-tonmoy007 <tonmoyhossainjifat313@gmail.com>
2023-07-01 07:53:10 +0600

    added routing functionality

--- src/App.js
+++ src/App.js
@@ -1,8 +1,25 @@
+import { BrowserRouter as Router, Routes, Route} from "react-router-dom";
+import Home from "./pages/Home";
+import ForgotPassword from "./pages/ForgotPassword";
+import Offers from "./pages/Offers";
+import Profile from "./pages/Profile";
+import SignUp from "./pages/SignUp";
+import Signin from "./pages/Signin";
+
+
 function App() {
   return (
-    <>
-      <h1 className='bg-red-400'>Hello world! </h1>
-    </>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home/>}></Route>
+        <Route path="/profile" element={<Profile/>}></Route>
+        <Route path="/offers" element={<Offers/>}></Route>
+        <Route path="/signup" element={<SignUp/>}></Route>
+        <Route path="/signin" element={<Signin/>}></Route>
+        <Route path="/forgotpassword" element={<ForgotPassword/>}></Route>
+        
+      </Routes>
+    </Router>
   );
 }
 
