b59d3fce038bf0921750556938857c4cbb6e845b
md-tonmoy007 <tonmoyhossainjifat313@gmail.com>
2023-07-02 20:16:28 +0600

    commit

--- src/pages/ForgotPassword.jsx
+++ src/pages/ForgotPassword.jsx
@@ -1,17 +1,25 @@
-import React, { useState } from 'react'
-import { AiFillEyeInvisible, AiFillEye } from "react-icons/ai";
-import { Link, useNavigate } from "react-router-dom";
-import OAuth from '../components/OAuth';
-
-
-
-
+import { getAuth, sendPasswordResetEmail } from "firebase/auth";
+import { useState } from "react";
+import { Link } from "react-router-dom";
+import { toast } from "react-toastify";
+import OAuth from "../components/OAuth";
 
 export default function ForgotPassword() {
-  const [email, setemail] = useState("")
+  const [email, setEmail] = useState("");
 
-  function onChange(e){
-    setemail(e.target.value);
+  function onChange(e) {
+    setEmail(e.target.value);
+  }
+
+  async function onSubmit(e) {
+    e.preventDefault();
+    try {
+      const auth = getAuth();
+      await sendPasswordResetEmail(auth, email);
+      toast.success("Email was sent");
+    } catch (error) {
+      toast.error("Could not send reset password");
+    }
   }
   return (
     <section>
@@ -25,7 +33,7 @@ export default function ForgotPassword() {
           />
         </div>
         <div className="w-full md:w-[67%] lg:w-[40%] lg:ml-20">
-          <form >
+          <form onSubmit={onsubmit}>
             <input
               type="email"
               id="email"
