cebffba1c404bbb3d86bc002ba1f5d608cd99389
md-tonmoy007 <tonmoyhossainjifat313@gmail.com>
2023-07-02 15:39:42 +0600

    commit

--- src/App.js
+++ src/App.js
@@ -6,10 +6,13 @@ import Profile from "./pages/Profile";
 import SignUp from "./pages/SignUp";
 import Signin from "./pages/Signin";
 import Header from "./components/Header";
+import { ToastContainer , toast} from "react-toastify";
+import 'react-toastify/dist/ReactToastify.css';
 
 
 function App() {
   return (
+    <>
     <Router>
       <Header/>
       <Routes>
@@ -18,10 +21,23 @@ function App() {
         <Route path="/offers" element={<Offers/>}></Route>
         <Route path="/sign-up" element={<SignUp/>}></Route>
         <Route path="/signin" element={<Signin/>}></Route>
-        <Route path="/forgot-password" element={<ForgotPassword/>}></Route>
-        
+        <Route path="/forgot-password" element={<ForgotPassword/>}>
+        </Route>
       </Routes>
     </Router>
+    <ToastContainer
+        position="bottom-center"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="dark"
+      />
+    </>
   );
 }
 
--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -2,6 +2,16 @@ import React, { useState } from 'react'
 import { AiFillEyeInvisible, AiFillEye } from "react-icons/ai";
 import { Link, useNavigate } from "react-router-dom";
 import OAuth from '../components/OAuth';
+import {
+  getAuth,
+  createUserWithEmailAndPassword,
+  updateProfile,
+} from "firebase/auth";
+import { db } from "../firebase";
+import { doc, serverTimestamp, setDoc } from 'firebase/firestore';
+import { toast } from 'react-toastify';
+
+
 
 export default function SignUp() {
   const [showPass, setShowPass] = useState(false)
@@ -11,6 +21,7 @@ export default function SignUp() {
     password: "",
   });
   const { name, email, password } = formData;
+  const navigate = useNavigate();
 
 
   function onChange(e){
@@ -20,6 +31,33 @@ export default function SignUp() {
     }));
   }
 
+  async function onSubmit(e) {
+    e.preventDefault();
+
+    try {
+      const auth = getAuth();
+      const userCredential = await createUserWithEmailAndPassword(
+        auth,
+        email,
+        password
+      );
+
+      updateProfile(auth.currentUser, {
+        displayName: name,
+      });
+      const user = userCredential.user;
+      const formDataCopy = { ...formData };
+      // delete formDataCopy.password;
+      formDataCopy.timestamp = serverTimestamp();
+
+      await setDoc(doc(db, "users", user.uid), formDataCopy);
+      toast.success("Sign up was successful");
+      navigate("/");
+    } catch (error) {
+      console.log(error)
+      toast.error("Something went wrong with the registration");
+    }
+  }
 
   return (
     <section>
@@ -33,7 +71,7 @@ export default function SignUp() {
             />
           </div>
           <div className="w-full md:w-[67%] lg:w-[40%] lg:ml-20">
-            <form>
+            <form onSubmit={onSubmit}>
             <input
                 type="text"
                 id="name"
